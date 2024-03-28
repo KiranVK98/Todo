07da32c51dac93b89debc56c6cd760e2f29ff534
Kiran Kulkarni <34923686+KiranVK98@users.noreply.github.com>
2024-03-28 04:02:34 -0500

    added creating todo- working

--- Backend/dot-back/obj/Debug/net8.0/dot-back.AssemblyInfo.cs
+++ Backend/dot-back/obj/Debug/net8.0/dot-back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dot-back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7956d15fd1855a41a2334d8a011fd8d0a532f185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74eab127ccc0ee03d5c7822a27920a6acb23723")]
 [assembly: System.Reflection.AssemblyProductAttribute("dot-back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dot-back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
